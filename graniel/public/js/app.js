3586afd2dff4630fabad4f50c36ad6978ff6e4fd
Ramiro Salinas <ramiro.salinas.silva@accenture.com>
2018-06-20 11:02:04 -0500

    adding scrollspy

--- graniel/public/js/app.js
+++ graniel/public/js/app.js
@@ -7,13 +7,8 @@ $( document ).ready(function() {
         event.preventDefault();
         var $section = $($(this).attr('href')); 
         $('html, body').animate({
-            scrollTop: $section.offset().top - 50
-        }, 700);
+            scrollTop: $section.offset().top - 20
+        }, 300);
     }
     $('[data-scroll]').on('click', scrollToSection);
-
-    $('.navbar .nav-link').click(function () {
-        $('.navbar .nav-link').removeClass('active');
-        $(this).addClass('active');
-     });
 });
