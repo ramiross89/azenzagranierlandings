805175d64d9124c010c5265cc572cb320a37f478
Ramiro Salinas <ramiro.salinas.silva@accenture.com>
2018-06-20 00:50:19 -0500

    build v1

---
+++ graniel/public/js/app.js
@@ -0,0 +1,19 @@
+  // Scroll to the desired section on click
+  // Make sure to add the `data-scroll` attribute to your `<a>`          tag.
+  // Example: 
+  // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
+
+$( document ).ready(function() {
+    $('.carousel').carousel({
+        interval: 1000
+    });
+    
+    function scrollToSection(event) {
+        event.preventDefault();
+        var $section = $($(this).attr('href')); 
+        $('html, body').animate({
+            scrollTop: $section.offset().top - 50
+        }, 700);
+    }
+    $('[data-scroll]').on('click', scrollToSection);
+});
